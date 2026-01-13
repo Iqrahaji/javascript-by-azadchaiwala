1a84ce68f403ddac8699d1d4a96ba60285e65cae
iqrabibi <iqrahaji3946@gmail.com>
2026-01-13 17:41:28 +0500

    next lessons added.

---
+++ Lesson no 02/Datatypes/Strings/strings.js
@@ -0,0 +1,12 @@
+let myName =  "Iqra haji";
+console.log(myName);
+console.log(typeof myName);
+
+// the value in double quotes considered as strings while without our other datatypes means 
+// it is number
+let totalSiblings = 6;
+console.log(totalSiblings);
+console.log(typeof totalSiblings);
+
+
+
---
+++ Lesson no 02/Datatypes/variables/const/const.js
@@ -0,0 +1,8 @@
+const totalStudents = 120;
+
+// const only use when we have to choose the real amount with accuracy.
+// it is not flexible 
+// scope will be declared in next lessons
+
+const rainbowColors = 7;
+const totalSiblings = 6;
