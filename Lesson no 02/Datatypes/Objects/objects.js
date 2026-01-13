dcd49ed00b6b19a79f34e9bf70091f5144f8bb26
iqrabibi <iqrahaji3946@gmail.com>
2026-01-13 18:27:02 +0500

    The Object lesson is added.

---
+++ Lesson no 02/Datatypes/Objects/objects.js
@@ -0,0 +1,18 @@
+let motorBike = {
+    model: 2017,
+    company: "Honda",
+    color: "Black and red"
+};
+
+console.log(motorBike);
+console.log(typeof motorBike);
+
+
+let user = {
+    name: "Iqra haji",
+    age: 20,
+    city: "Lahore"
+};
+
+console.log(user);
+console.log(typeof user);
