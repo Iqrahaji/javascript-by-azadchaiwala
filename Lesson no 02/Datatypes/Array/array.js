288928934e372aeb44f2ca736628c833b21956f0
iqrabibi <iqrahaji3946@gmail.com>
2026-01-13 18:00:26 +0500

    add next lessons.

---
+++ Lesson no 02/Datatypes/Array/array.js
@@ -0,0 +1,14 @@
+let fruits = ["apple", "banana", "mango"];
+
+console.log(fruits);
+console.log(typeof fruits)
+
+let rainbowColors = ["red", "green", "blue", "indigo", "violet", "yellow", "orange"];
+
+console.log9(rainbowColors);
+console.log(typeof rainbowColors);
+
+let vegetables = ["onion", "potato", "tomato", "garlic"];
+
+console.log(vegetables);
+console.log(typeof vegetables);
---
+++ Lesson no 02/Datatypes/Boolean/boolean.js
@@ -0,0 +1,12 @@
+let isStudents = true;
+
+console.log(isStudents);
+console.log(typeof isStudents);
+
+
+let isStudent = false;
+
+console.log(isStudent);
+console.log(typeof isStudent);
+
+
