a991e890a05e7da314c86fdc155076cb79899dfd
iqrabibi <iqrahaji3946@gmail.com>
2026-01-14 14:16:31 +0500

    The functions basic information.

---
+++ Lesson no 02/Datatypes/Functions/functions.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+let result = add(5, 3);
+console.log(result);
+
+function substract(a, b) {
+  return a - b;
+}
+
+let remarks = substract(5, 3);
+console.log(remarks);
+
+
+function multiply(a, b) {
+  return a * b;
+}
+
+let retain = multiply(5, 3);
+console.log(retain);
+
