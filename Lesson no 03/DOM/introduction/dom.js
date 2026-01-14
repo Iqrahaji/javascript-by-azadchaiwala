e401ee4b0d5ac4985524219138f5ee6da251671e
iqrabibi <iqrahaji3946@gmail.com>
2026-01-14 16:37:11 +0500

    some code make as commits.

--- Lesson no 03/DOM/introduction/dom.js
+++ Lesson no 03/DOM/introduction/dom.js
@@ -4,5 +4,8 @@ let clickIt = document.getElementById("click");
 console.log(clickIt.innerText)
 console.log(clickIt.innerHTML)
 
-let para1 = document.getElementsByClassName('para1');
+// let para1 = document.getElementsByClassName('para1');
+//  para1.style.color = "red"
+//  para1.style.backgroundColor = "black"
+
 
