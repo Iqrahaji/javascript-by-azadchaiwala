8fc1d6e33afc7dd3e6236563a0a27899bea7c74b
iqrabibi <iqrahaji3946@gmail.com>
2026-01-16 15:43:48 +0500

    The selectors lesson started.

---
+++ Lesson no 03/DOM/Selectors and manipulating Html/selectors.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('btn');
+
+console.log(btn.innerHTML)
+console.log(btn.innerText)
+// How to manipulate 
+// manipulate inner text content
+
+document.getElementById('btn').textContent = "Click the button." ;
+
